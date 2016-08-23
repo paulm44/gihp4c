3efadccd69240d46cd5e99e7a534e1219ee2a6a4
paulm44 <paulandres44@hotmail.com>
2016-08-23 15:20:10 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(function() {
 
 	var app_id = '507970359393580';
-	var scopes = 'email, user_friends, user_online_presence';
+	var scopes = 'email, user_friends, public_profile';
 
 	var btn_login = '<a href="#" id="login" class="btn btn-primary">Iniciar sesión</a>';
 
