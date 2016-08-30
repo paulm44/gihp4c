1d6a18fe989d4131d608292350109403d6012ebb
paulm44 <paulandres44@hotmail.com>
2016-08-30 12:50:22 -0500

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
- 
-$CONSUMER_KEY='ie8rTFolnhROyqW3KFBhFWv8H';
-$CONSUMER_SECRET='NLi8dgAJUjijaX22HhVZvLD2cQgonQLhjsMVniQ42d1zKbnYtb';
-$OAUTH_CALLBACK='https://paulm44.github.io/gihp4c/';
- 
-?>
