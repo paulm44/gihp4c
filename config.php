e1539053fb8580c9c9e43f2a42b1afdbf4cbcabc
paulm44 <paulandres44@hotmail.com>
2016-08-30 12:42:57 -0500

    Create config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+ 
+$CONSUMER_KEY='ie8rTFolnhROyqW3KFBhFWv8H';
+$CONSUMER_SECRET='NLi8dgAJUjijaX22HhVZvLD2cQgonQLhjsMVniQ42d1zKbnYtb';
+$OAUTH_CALLBACK='https://paulm44.github.io/gihp4c/';
+ 
+?>
