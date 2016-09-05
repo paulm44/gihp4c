8bb45b012a64456e2ee893b8f130b3fa44a8b378
paulm44 <paulandres44@hotmail.com>
2016-09-05 14:33:32 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,107 +1,95 @@
-<?php
-/*  TWITTER LOGIN BASIC - Tutorial
- *  file            - index.php
- *  Developer       - Krishna Teja G S
- *  Website         - http://packetcode.com/apps/twitter-login/
- *  Date            - 4th Sept 2015
- *  license         - GNU General Public License version 2 or later
- */
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login GIHP4C</title>
+	<meta charset="UTF-8">
+	<meta name="google-signin-scope" content="profile email">
+    	<meta name="google-signin-client_id" content="831510361387-
 
-/* 
- * CODE BREAKDOWN
- *   PART 1 - DEFINING (loads files,global constants,session enabling)
- *   PART 2 - PROCESS ( check for logout,user session,call back request ) 
- *   PART 3 - FRONT END (display login url or user data)
- *
- */
+lkko2b6td272guqrup0ph3d9nejojlps.apps.googleusercontent.com">
+    	<script src="https://apis.google.com/js/platform.js" async defer></script>
+</head>
+<body>
+	// Google+	
+	<script>
+	      function onSignIn(googleUser) {
+	        // Useful data for your client-side scripts:
+	        var profile = googleUser.getBasicProfile();
+	        console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+	        console.log('Full Name: ' + profile.getName());
+	        console.log('Given Name: ' + profile.getGivenName());
+	        console.log('Family Name: ' + profile.getFamilyName());
+	        console.log("Image URL: " + profile.getImageUrl());
+	        console.log("Email: " + profile.getEmail());
+	        // The ID token you need to pass to your backend:
+	        var id_token = googleUser.getAuthResponse().id_token;
+	        console.log("ID Token: " + id_token);
+	      };
+	</script>
 
+	// Facebook
+	<script>
+	    function statusChangeCallback(response) {
+	    console.log('statusChangeCallback');
+	    console.log(response);
+	    
+	    if (response.status === 'connected') {
+	      // Logged into your app and Facebook.
+	      testAPI();
+	    } else if (response.status === 'not_authorized') {
+	      document.getElementById('status').innerHTML = 'Please log ' +
+	        'into this app.';
+	    } else {
+	      document.getElementById('status').innerHTML = 'Please log ' +
+	        'into Facebook.';
+	    }
+	  }
+	
+	  function checkLoginState() {
+	    FB.getLoginStatus(function(response) {
+	      statusChangeCallback(response);
+	    });
+	  }
+	  window.fbAsyncInit = function() {
+	  FB.init({
+	    appId      : '507970359393580',
+	    cookie     : true,  // enable cookies to allow the server to access 
+	    status     : true,                // the session
+	    xfbml      : true,  // parse social plugins on this page
+	    version    : 'v2.5' // use graph api version 2.5
+	  });
+	
+	  FB.getLoginStatus(function(response) {
+	    statusChangeCallback(response);
+	  });
+	  };
+	  // Load the SDK asynchronously
+	  (function(d, s, id) {
+	    var js, fjs = d.getElementsByTagName(s)[0];
+	    if (d.getElementById(id)) return;
+	    js = d.createElement(s); js.id = id;
+	    js.src = "//connect.facebook.net/en_US/sdk.js";
+	    fjs.parentNode.insertBefore(js, fjs);
+	  }(document, 'script', 'facebook-jssdk'));
+	
+	  function testAPI() {
+	    console.log('Welcome!  Fetching your information.... ');
+	    FB.api('/me', function(response) {
+	      console.log('Successful login for: ' + response.name);
+	      document.getElementById('status').innerHTML =
+	        'Thanks for logging in, ' + response.name + '!';
+	    });
+	  }
+	</script>
 
-/* 
- * PART 1 - DEFINING 
- */
-// Load the library files
-require_once('twitteroauth/OAuth.php');
-require_once('twitteroauth/twitteroauth.php');
-// define the consumer key and secet and callback
-define('CONSUMER_KEY', 'ie8rTFolnhROyqW3KFBhFWv8H');
-define('CONSUMER_SECRET', 'NLi8dgAJUjijaX22HhVZvLD2cQgonQLhjsMVniQ42d1zKbnYtb');
-define('OAUTH_CALLBACK', 'https://paulm44.github.io/gihp4c/');
-// start the session
-session_start();
+<fb:login-button scope="public_profile,email" onlogin="checkLoginState();">
+</fb:login-button>
+<div id="status">
+</div>
 
-/* 
- * PART 2 - PROCESS
- * 1. check for logout
- * 2. check for user session  
- * 3. check for callback
- */
+<br><br><br>
 
-// 1. to handle logout request
-if(isset($_GET['logout'])){
-	//unset the session
-	session_unset();
-	// redirect to same page to remove url paramters
-	$redirect = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
-  	header('Location: ' . filter_var($redirect, FILTER_SANITIZE_URL));
-}
+<div class="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div>
 
-
-// 2. if user session not enabled get the login url
-if(!isset($_SESSION['data']) && !isset($_GET['oauth_token'])) {
-	// create a new twitter connection object
-	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET);
-	// get the token from connection object
-	$request_token = $connection->getRequestToken(OAUTH_CALLBACK); 
-	// if request_token exists then get the token and secret and store in the session
-	if($request_token){
-		$token = $request_token['oauth_token'];
-		$_SESSION['request_token'] = $token ;
-		$_SESSION['request_token_secret'] = $request_token['oauth_token_secret'];
-		// get the login url from getauthorizeurl method
-		$login_url = $connection->getAuthorizeURL($token);
-	}
-}
-
-// 3. if its a callback url
-if(isset($_GET['oauth_token'])){
-	// create a new twitter connection object with request token
-	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $_SESSION['request_token'], $_SESSION['request_token_secret']);
-	// get the access token from getAccesToken method
-	$access_token = $connection->getAccessToken($_REQUEST['oauth_verifier']);
-	if($access_token){	
-		// create another connection object with access token
-		$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
-		// set the parameters array with attributes include_entities false
-		$params =array('include_entities'=>'false');
-		// get the data
-		$data = $connection->get('account/verify_credentials',$params);
-		if($data){
-			// store the data in the session
-			$_SESSION['data']=$data;
-			// redirect to same page to remove url parameters
-			$redirect = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
-  			header('Location: ' . filter_var($redirect, FILTER_SANITIZE_URL));
-		}
-	}
-}
-
-/* 
- * PART 3 - FRONT END 
- *  - if userdata available then print data
- *  - else display the login url
-*/
-
-if(isset($login_url) && !isset($_SESSION['data'])){
-	// echo the login url
-	echo "<a href='$login_url'><button>Login with twitter </button></a>";
-}
-else{
-	// get the data stored from the session
-	$data = $_SESSION['data'];
-	// echo the name username and photo
-	echo "Name : ".$data->name."<br>";
-	echo "Username : ".$data->screen_name."<br>";
-	echo "Photo : <img src='".$data->profile_image_url."'/><br><br>";
-	// echo the logout button
-	echo "<a href='?logout=true'><button>Logout</button></a>";
-} 
+</body>
+</html>
