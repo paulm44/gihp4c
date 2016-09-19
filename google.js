3bdc2cbe92d4f9f67f335ff3c1909eb33c5a4e7f
paulm44 <paulandres44@hotmail.com>
2016-09-19 18:23:27 -0500

    Update google.js

--- google.js
+++ google.js
@@ -1,14 +1,28 @@
-
-  function onSignIn(googleUser) {
-    // Useful data for your client-side scripts:
-    var profile = googleUser.getBasicProfile();
-    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-    console.log('Full Name: ' + profile.getName());
-    console.log('Given Name: ' + profile.getGivenName());
-    console.log('Family Name: ' + profile.getFamilyName());
-    console.log("Image URL: " + profile.getImageUrl());
-    console.log("Email: " + profile.getEmail());
-    // The ID token you need to pass to your backend:
-    var id_token = googleUser.getAuthResponse().id_token;
-    console.log("ID Token: " + id_token);
+var googleUser = {};
+  var startApp = function() {
+    gapi.load('auth2', function(){
+      // Retrieve the singleton for the GoogleAuth library and set up the client.
+      auth2 = gapi.auth2.init({
+        client_id: '831510361387-lkko2b6td272guqrup0ph3d9nejojlps.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+        // Request scopes in addition to 'profile' and 'email'
+        //scope: 'additional_scope'
+      });
+      attachSignin(document.getElementById('customBtn'));
+    });
   };
+
+
+  function attachSignin(element) {
+     
+     console.log(element.id);
+     auth2.attachClickHandler(element, {}, function(googleUser){
+      document.getElementById('name').innerText="Nombre: " + googleUser.getBasicProfile().getName();
+      document.getElementById('correo').innerText="Email: " + googleUser.getBasicProfile().getEmail();
+      document.getElementById('imagen').innerText="Imagen: " + googleUser.getBasicProfile().getImageUrl();
+     },
+     function(error) {
+      alert(JSON.stringify(error, undefined, 2));
+     });
+
+  }
